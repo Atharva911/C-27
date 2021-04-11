2c080f90b30216df30b0743bd5c7d4f7d47a84e5
Atharva <procuber27@gmail.com>
2021-04-11 12:28:50 +0530

    created sling

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const constrain = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var meet;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,12 +38,16 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6 = new Log(230,180,80,PI/2)
+    meet = new Join(bird.body ,log6.body)
 
 }
-
 function draw(){
-    background(backgroundImg);
+    background(backgroundImg)
     Engine.update(engine);
+
+   
+
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
@@ -61,4 +68,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    meet.display();
 }
+
